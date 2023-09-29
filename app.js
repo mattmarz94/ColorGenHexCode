aa4b9fd4b479496623ffa0e8bd20d8887f9c63cc
Matt Marzano <122555133+mattmarz94@users.noreply.github.com>
2023-09-29 16:42:24 -0400

    added some styling to the color generator.

--- app.js
+++ app.js
@@ -1,41 +1,38 @@
-function colorSelector(){
-       let selectedColor = document.getElementById("color-selector").value
-       let colorsArray = selectedColor.split("");
-       colorsArray.shift()
-       let hexArray = colorsArray.join("")
-       return hexArray
+function colorSelector() {
+  let selectedColor = document.getElementById("color-selector").value;
+  let colorsArray = selectedColor.split("");
+  colorsArray.shift();
+  let hexArray = colorsArray.join("");
+  return hexArray;
 }
 
-function modeSelector(){
-       let colorScheme = document.getElementById("color-scheme").value
-       return colorScheme
+function modeSelector() {
+  let colorScheme = document.getElementById("color-scheme").value;
+  return colorScheme;
 }
 
-function renderColors(data){
+function renderColors(data) {
+  let chosenColers = "";
+  let hexNumbers = "";
 
-       let chosenColers = ""
-       let hexNumbers = ""
-
-       for (let color of data.colors){
-              chosenColers += `
+  for (let color of data.colors) {
+    chosenColers += `
               <div class = "render-colors" style="background-color: ${color.hex.value}"></div>
-              `
-              hexNumbers += `
+              `;
+    hexNumbers += `
               <div class = "render-hex">${color.hex.value}</div>
-              `
-       }
-       document.getElementById("scheme-container").innerHTML = chosenColers
-       document.getElementById("scheme-color-info").innerHTML = hexNumbers
+              `;
+  }
+  document.getElementById("scheme-container").innerHTML = chosenColers;
+  document.getElementById("scheme-color-info").innerHTML = hexNumbers;
 }
 
 document.getElementById("get-scheme-btn").addEventListener("click", (e) => {
+  fetch(
+    `https://www.thecolorapi.com/scheme?hex=${colorSelector()}&mode=${modeSelector()}`
+  )
+    .then((res) => res.json())
+    .then((data) => renderColors(data));
 
-       fetch(`https://www.thecolorapi.com/scheme?hex=${colorSelector()}&mode=${modeSelector()}`)
-              .then(res => res.json())
-              .then(data => renderColors(data))
-   
-       e.preventDefault()
-})
-
-
-    
+  e.preventDefault();
+});
