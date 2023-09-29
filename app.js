cf1f221f32d8ee6201af40db5ff1f0740f736f2a
Matt Marzano <122555133+mattmarz94@users.noreply.github.com>
2023-09-29 00:22:20 -0400

    reformatted the HTML and JS using Prettier, in the JS i created a renderColors function in order to render colors onto the HTML. I iterated through the data, and lastly added the created functions into the fetch method and placed the render function inside the .then function to be called.

--- app.js
+++ app.js
@@ -2,22 +2,37 @@ function colorSelector(){
        let selectedColor = document.getElementById("color-selector").value
        let colorsArray = selectedColor.split("");
        colorsArray.shift()
-       // console.log(colorsArray.shift())
        let hexArray = colorsArray.join("")
-       // console.log(hexArray)
        return hexArray
 }
 
 function modeSelector(){
        let colorScheme = document.getElementById("color-scheme").value
-       console.log(colorScheme)
+       return colorScheme
+}
+
+function renderColors(data){
+
+       let chosenColers = ""
+       let hexNumbers = ""
+
+       for (let color of data.colors){
+              chosenColers += `
+              <div class = "render-colors" style="background-color: ${color.hex.value}"></div>
+              `
+              hexNumbers += `
+              <div class = "render-hex">${color.hex.value}</div>
+              `
+       }
+       document.getElementById("scheme-container").innerHTML = chosenColers
+       document.getElementById("scheme-color-info").innerHTML = hexNumbers
 }
 
 document.getElementById("get-scheme-btn").addEventListener("click", (e) => {
 
-       fetch(`https://www.thecolorapi.com/scheme?hex=${colorSelector}&mode=${modeSelector}`)
+       fetch(`https://www.thecolorapi.com/scheme?hex=${colorSelector()}&mode=${modeSelector()}`)
               .then(res => res.json())
-              .then(data => console.log(data))
+              .then(data => renderColors(data))
    
        e.preventDefault()
 })
