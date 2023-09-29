93cc7b13cee12962a7940be38e3a64733e6157b3
Matt Marzano <122555133+mattmarz94@users.noreply.github.com>
2023-09-28 23:25:10 -0400

    created function colorSelector in order to grab the selected color value, and remove the # symbol for the color code so it can GET the correct colors from the API.

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-document.getElementById("get-scheme").addEventListener("click", () => {
-    
+const colorScheme = document.getElementById("color-scheme")
+
+function colorSelector(){
+       let selectedColor = document.getElementById("color-selector").value
+       let colorsArray = selectedColor.split("");
+       colorsArray.shift()
+       // console.log(colorsArray.shift())
+       let hexArray = colorsArray.join("")
+       console.log(hexArray)
+}
+colorSelector()
+
+document.getElementById("get-scheme-btn").addEventListener("click", (e) => {
+
+       fetch(`https://www.thecolorapi.com/scheme?hex=${colorSelector}`)
+              .then(res => res.json())
+              .then(data => console.log(data))
    
+       e.preventDefault()
 })
 
- fetch("https://www.thecolorapi.com/scheme", {method: "GET"})
-        .then(res => res.json())
-        .then(data => console.log(data))
+
     
